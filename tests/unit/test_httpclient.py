84a71080e4e2869ea42767b11417c0d22dd5a820
Xe1ga <frenzied89@mail.ru>
2020-11-24 15:54:48 +0300

    Новый тест test_get_response_data_200_ok

--- tests/unit/test_httpclient.py
+++ tests/unit/test_httpclient.py
@@ -1,40 +1,44 @@
 import pytest
 
+from jsonschema import validate
 from unittest.mock import patch, Mock
 
-from repository_statistics.httpclient import get_response_content_with_pagination, _get_response, requests
+from repository_statistics.httpclient import (get_response_content_with_pagination,
+                                              _get_response, requests, get_response_data)
 from repository_statistics.structure import ResponseData
 from repository_statistics.exceptions import TimeoutConnectionError, ConnectError, HTTPError
 
 
-request_attributes = [
-    ('https://good/url',
-     {'sha': 'master', 'since': '2020-10-01T00:00:00', 'until': '2020-10-31T23:59:59.999999', 'per_page': '100'},
-     {'Accept': 'application/vnd.github.v3+json', 'Authorization': 'Token'},
-     {"created_at": "date", "author": {"login": "max-ott"}}
-     )
-]
+request_url = "http://url"
+request_parameters = {'sha': 'master', 'since': '2020-10-01T00:00:00', 'until': '2020-10-31T23:59:59.999999', 'per_page': '100'}
+request_headers = {'Accept': 'application/vnd.github.v3+json', 'Authorization': 'Token'}
+response_text = {"created_at": "date", "author": {"login": "name"}}
 
-request_attributes_with_method = [
-    ('https://good/url',
-     'get',
-     {'Accept': 'application/vnd.github.v3+json', 'Authorization': 'Token'},
-     {'sha': 'master', 'since': '2020-10-01T00:00:00', 'until': '2020-10-31T23:59:59.999999', 'per_page': '100'}
-     ),
-    ('https://good/url',
-     'head',
-     {'Accept': 'application/vnd.github.v3+json', 'Authorization': 'Token'},
-     {'sha': 'master', 'since': '2020-10-01T00:00:00', 'until': '2020-10-31T23:59:59.999999', 'per_page': '100'}
-     )
 
-]
+request_attributes = [(request_url, request_parameters, request_headers)]
+
+
+request_attributes_with_result = [(request_url, request_parameters, request_headers, response_text)]
+
+
+request_attributes_with_method = [(request_url, "get", request_parameters, request_headers),
+                                  (request_url, "head", request_parameters, request_headers)]
+
+
+mock_status_200 = Mock(status_code=200, json={"created_at": "date", "author": {"login": "name"}})
+
+
+valid_schema = {"title": "response",
+                "description": "response json",
+                "type": "object",
+                "properties": {"created_at": {"type": "string"},
+                               "author": {"login": {"type": "string"}}},
+                "required": ["created_at"]
+}
 
 
 def get_structure_response_data(*args, **kwargs):
-    response_json = [{
-        "created_at": "date",
-        "author": {"login": "max-ott"},
-    }]
+    response_json = [response_text]
     links = dict(next={"url": "good"})
     return ResponseData(
         response_json=response_json,
@@ -45,14 +49,14 @@ def get_structure_response_data(*args, **kwargs):
     )
 
 
-@pytest.mark.parametrize('url, parameters, headers, result', request_attributes)
+@pytest.mark.parametrize('url, parameters, headers, result', request_attributes_with_result)
 def test_get_response_content_with_pagination(url, parameters, headers, result):
     with patch('repository_statistics.httpclient.get_response_data') as mock_get_response_data:
         with patch('repository_statistics.httpclient.get_next_pages') as mock_get_next_pages:
             mock_get_response_data.side_effect = get_structure_response_data
             mock_get_next_pages.return_value = ""
-            parameters = (url, parameters, headers)
-            data = get_response_content_with_pagination(parameters)
+            params = (url, parameters, headers)
+            data = get_response_content_with_pagination(params)
             assert next(data) == result
             with pytest.raises(StopIteration):
                 next(data)
@@ -83,10 +87,18 @@ def test_get_response_http_err(mock_requests_get, mock_requests_head, url, metho
 
 
 @pytest.mark.parametrize('url, method, parameters, headers', request_attributes_with_method)
-@patch.object(requests, 'head', return_value=Mock(status_code=200,
-                                                  json={"created_at": "date", "author": {"login": "max-ott"}}))
-@patch.object(requests, 'get', return_value=Mock(status_code=200,
-                                                 json={"created_at": "date", "author": {"login": "max-ott"}}))
-def test_get_response(mock_requests_get, mock_requests_head, url, method, parameters, headers):
+@patch.object(requests, 'head', return_value=mock_status_200)
+@patch.object(requests, 'get', return_value=mock_status_200)
+def test_get_response_200_ok(mock_requests_get, mock_requests_head, url, method, parameters, headers):
     response = _get_response(url, method, parameters, headers)
-    assert response.json == {"created_at": "date", "author": {"login": "max-ott"}}
+    assert response.json == response_text
+
+
+@pytest.mark.parametrize('url, parameters, headers', request_attributes)
+@patch('repository_statistics.httpclient._get_response', return_value=Mock(status_code=200))
+def test_get_response_data_200_ok(mock_response, url, parameters, headers):
+    mock_response.return_value.json.return_value = response_text
+    mock_response.return_value.links = {}
+    mock_response.return_value.headers = {}
+    response_data = get_response_data(url, parameters, headers)
+    validate(response_data.response_json, valid_schema)
